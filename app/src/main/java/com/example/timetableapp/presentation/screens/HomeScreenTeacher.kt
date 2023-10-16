cea292f781304323830853d2edf10dc1a457b02d
Delma <delmant1985@gmail.com>
2023-10-16 12:05:28 +0300

    added the first implementation of new screens

--- app/src/main/java/com/example/timetableapp/presentation/screens/HomeScreenTeacher.kt
+++ app/src/main/java/com/example/timetableapp/presentation/screens/HomeScreenTeacher.kt
@@ -1,28 +1,24 @@
-package com.example.timetableapp.presentation.screens.welcome_screen
+package com.example.timetableapp.presentation.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.timetableapp.presentation.components.AppIcon
-import com.example.timetableapp.presentation.components.ButtonWithoutIcon
-import com.example.timetableapp.presentation.components.GradientMediumText
-import com.example.timetableapp.presentation.components.GradientSmallText
-import com.example.timetableapp.presentation.components.RegularText
-import com.example.timetableapp.presentation.components.TextField
+import com.example.timetableapp.presentation.screens.components.AppIcon
+import com.example.timetableapp.presentation.screens.components.ButtonWithoutIcon
+import com.example.timetableapp.presentation.screens.components.GradientSmallText
+import com.example.timetableapp.presentation.screens.components.SpacerHeight20dp
 
-@Preview
 @Composable
-fun HomeScreen( ) {
+fun HomeScreenTeacher(
+    onPersonalTimetableClickListener: () -> Unit,
+    onGroupTimetableClickListener: () -> Unit
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -35,6 +31,13 @@ fun HomeScreen( ) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             GradientSmallText(text = "\uD83C\uDF03Доброй ночи, Удрас Д.Н.")
+            SpacerHeight20dp()
+            ButtonWithoutIcon(text = "Своё расписание") {
+                onPersonalTimetableClickListener()
+            }
+            ButtonWithoutIcon(text = "Расписание группы") {
+                onGroupTimetableClickListener()
+            }
         }
     }
     AppIcon()
