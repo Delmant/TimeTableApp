6395088967815b0b36598ea4bb8d54bec03c3571
Delma <delmant1985@gmail.com>
2023-10-16 12:00:38 +0300

    added 4 new screens to the seal class screen

--- app/src/main/java/com/example/timetableapp/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/timetableapp/presentation/navigation/Screen.kt
@@ -14,14 +14,29 @@ sealed class Screen(val route: String) {
         route = ROUTE_CHOOSE_TEACHER_SCREEN
     )
 
-    object HomeScreen : Screen(
-        route = ROUTE_HOME_SCREEN
+    object HomeScreenTeacher : Screen(
+        route = ROUTE_HOME_SCREEN_STUDENT
+    )
+
+    object HomeScreenStudent : Screen(
+        route = ROUTE_HOME_SCREEN_TEACHER
+    )
+
+    object GroupAndDateSelectionScreen: Screen(
+        route = ROUTE_GROUP_AND_DATE_SELECTION_SCREEN
+    )
+
+    object DateSelectionScreen: Screen(
+        route = ROUTE_DATE_SELECTION_SCREEN
     )
 
     companion object {
         private const val ROUTE_WELCOME_SCREEN = "route_welcome_screen"
         private const val ROUTE_CHOOSE_STUDENT_SCREEN = "route_choose_student_screen"
         private const val ROUTE_CHOOSE_TEACHER_SCREEN = "route_choose_teacher_screen"
-        private const val ROUTE_HOME_SCREEN = "route_home_screen"
+        private const val ROUTE_HOME_SCREEN_STUDENT = "route_home_screen_student"
+        private const val ROUTE_HOME_SCREEN_TEACHER = "route_home_screen_teacher"
+        private const val ROUTE_GROUP_AND_DATE_SELECTION_SCREEN = "route_group_and_date_selection_screen"
+        private const val ROUTE_DATE_SELECTION_SCREEN = "route_date_selection_screen"
     }
 }
