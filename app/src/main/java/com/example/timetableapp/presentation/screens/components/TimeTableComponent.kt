ca7a53d16eb418e1924d384d4b951035ed408d05
Delma <delmant1985@gmail.com>
2023-10-16 12:03:00 +0300

    added a test implementation of the drop down menu

--- app/src/main/java/com/example/timetableapp/presentation/screens/components/TimeTableComponent.kt
+++ app/src/main/java/com/example/timetableapp/presentation/screens/components/TimeTableComponent.kt
@@ -1,4 +1,4 @@
-package com.example.timetableapp.presentation.components
+package com.example.timetableapp.presentation.screens.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -10,17 +10,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
