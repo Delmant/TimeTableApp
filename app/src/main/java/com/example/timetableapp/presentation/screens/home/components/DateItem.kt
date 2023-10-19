237c9e2bdb15eb95a395cc28992356b31d1d1fae
Delma <delmant1985@gmail.com>
2023-10-19 12:13:36 +0300

    created lazy row for a date

---
+++ app/src/main/java/com/example/timetableapp/presentation/screens/home/components/DateItem.kt
@@ -0,0 +1,43 @@
+package com.example.timetableapp.presentation.screens.home.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.timetableapp.presentation.screens.shared_components.SpacerHeight10dp
+import com.example.timetableapp.presentation.screens.shared_components.SpacerWidth20dp
+import com.example.timetableapp.presentation.screens.shared_components.WhiteFont16SP
+import com.example.timetableapp.presentation.screens.shared_components.WhiteFont26SP
+import com.example.timetableapp.ui.theme.MainBlue
+
+@Composable
+fun DateItem() {
+    Row {
+        Card(
+            shape = RoundedCornerShape(25.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(MainBlue)
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    WhiteFont16SP(text = "ПНД")
+                    SpacerHeight10dp()
+                    WhiteFont16SP(text = "20")
+                }
+            }
+        }
+        SpacerWidth20dp()
+    }
+}
