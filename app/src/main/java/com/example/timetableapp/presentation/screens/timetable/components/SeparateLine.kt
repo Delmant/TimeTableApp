c29ed6bbefc1edd5996638944abfc8562846eec7
Delma <delmant1985@gmail.com>
2023-10-19 12:17:35 +0300

    dividing line component

---
+++ app/src/main/java/com/example/timetableapp/presentation/screens/timetable/components/SeparateLine.kt
@@ -0,0 +1,24 @@
+package com.example.timetableapp.presentation.screens.timetable.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.timetableapp.presentation.screens.shared_components.SpacerHeight10dp
+
+@Composable
+fun SeparateLine() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(color = Color(0xFF0174F3))
+    ) {
+
+    }
+    SpacerHeight10dp()
+}
