7cf4b491573644af756718b4cb6a12d622e5f8de
Delma <delmant1985@gmail.com>
2023-10-31 14:14:16 +0300

    corrected navigation to the home screen

--- app/src/main/java/com/example/timetableapp/presentation/navigation/NavigationState.kt
+++ app/src/main/java/com/example/timetableapp/presentation/navigation/NavigationState.kt
@@ -2,6 +2,7 @@ package com.example.timetableapp.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -11,6 +12,14 @@ class NavigationState(
     fun navigateTo(route: String) {
         navHostController.navigate(route) {
             restoreState = true
+        }
+    }
+
+    fun navigateToHome(route: String) {
+        navHostController.navigate(route) {
+            popUpTo(navHostController.graph.findStartDestination().id) {
+                inclusive = true
+            }
         }
     }
 }
